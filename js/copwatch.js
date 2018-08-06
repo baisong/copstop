b662b39119b4cab160dd8040bfdcef5b1a99b52a
Oren Robinson <get.oren@gmail.com>
2018-08-06 15:03:00 -0700

    Fix js

--- js/copwatch.js
+++ js/copwatch.js
@@ -1,6 +1,5 @@
 // Go
 window.onload = function() {
-  Fireworks.initialize();
   var howLaunched = window.location.search.substring(1).split('=')[0];
   if (howLaunched === 'homescreen') {
     var welcome = document.getElementById('welcome-back');
